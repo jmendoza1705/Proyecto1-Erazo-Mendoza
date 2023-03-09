24f2a398710b9bc6cd955521336e83af9cf9aee0
jmendoza1705 <julianamendoza8@gmail.com>
2023-03-09 12:19:24 -0500

    Cambios Codigo Modelo

--- Modelo P1.py
+++ Modelo P1.py
@@ -38,11 +38,6 @@ for j in range(0, data.shape[0]):
     elif data[j, 13] == 4:
         data[j, 13] = 3
 
-## Se define el modelo
-
-# Se define la red bayesiana
-modelo_HD = BayesianNetwork([("AGE", "CHOL"), ("FBS", "CHOL"), ("CHOL", "HD"), ("CP", "HD"), ("HD", "THAL"), ("HD", "EXANG"),
-                         ("HD", "OLDPEAK")])
 
 ## Discretizacion del colesterol
 # menos de 200 -- Deseable
@@ -58,6 +53,45 @@ for j in range(0, data.shape[0]):
     elif data[j, 4] >= 240:
         data[j, 4] = 2
 
+## Discretización de OldPeak
+# Menos de 2 - 0
+# Entre 2 y 4 - 1
+# Mayor o igual a 4 - 2
+
+for j in range(0, data.shape[0]):
+    if data[j, 9] < 2:
+        data[j, 9] = 0
+    elif (2 <= data[j, 9] < 4):
+        data[j, 9] = 1
+    elif data[j, 9] >= 4:
+        data[j, 9] = 2
+
+
+## Discretización de la edad
+# 29 a 39 -- 30
+# 40 a 49 -- 40
+# 50 a 59 -- 50
+# 60 a 69 -- 60
+# Mayor o igual a 70 -- 70
+
+for j in range(0, data.shape[0]):
+    if (29 <= data[j, 0] < 40):
+        data[j, 0] = 30
+    elif (40 <= data[j, 0] < 50):
+        data[j, 0] = 40
+    elif (50 <= data[j, 0] < 60):
+        data[j, 0] = 50
+    elif (60 <= data[j, 0] < 70):
+        data[j, 0] = 60
+    elif data[j, 0] >= 70 :
+        data[j, 0] = 70
+
+## Se define el modelo
+
+# Se define la red bayesiana
+modelo_HD = BayesianNetwork([("AGE", "CHOL"), ("FBS", "CHOL"), ("CHOL", "HD"), ("CP", "HD"), ("THAL", "HD"), ("HD", "EXANG"),
+                         ("HD", "OLDPEAK")])
+##
 # Se definen las muestras
 info = np.zeros((296,8))
 columnas = [0, 2, 4, 5, 8, 9, 12, 13]
@@ -73,10 +107,10 @@ estimador_HD = MaximumLikelihoodEstimator(model=modelo_HD, data=muestras)
 modelo_HD.fit(data=muestras, estimator = MaximumLikelihoodEstimator)
 for i in modelo_HD.nodes():
     print("CPD ", i,"\n", modelo_HD.get_cpds(i))
-
+##
 #Se imprime completa la CDP HD
-for i in range(len(modelo_HD.get_cpds("HD").values)):
-    print(modelo_HD.get_cpds("HD").values[i])
+for i in range(len(modelo_HD.get_cpds("CHOL").values)):
+    print(modelo_HD.get_cpds("CHOL").values[i])
 
 ## FALTA PONER PSEUDOCOUNTS
 estimador = BayesianEstimator(model=modelo_HD, data=muestras)
